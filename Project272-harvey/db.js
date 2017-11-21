3f7a78aadb65cb00b0aeb7cb0326ee4fe0c9659d
Divya-Bharti <31812888+Divya-Bharti@users.noreply.github.com>
2017-11-20 22:53:09 -0800

    changes for profile page

--- Project272-harvey/db.js
+++ Project272-harvey/db.js
@@ -1,4 +1,4 @@
 module.exports = {
 	'url' : 'mongodb://divya:divya@ds149905.mlab.com:49905/project272'
-	//'url' : 'mongodb://localhost:27017/project272'
+//'url' : 'mongodb://localhost:27017/project272'
 }
